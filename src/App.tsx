8a7b01ce04d4fc215e9ec4fd8ec9c4d5892da4df
윤태현 <yoonth0919@gmail.com>
2023-11-14 23:05:49 +0900

    :recycle: Refactor: App.tsx Router 구조 개선

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "@pages/Main";
 import NotFound from "@pages/NotFound";
 import CategoryPage from "@pages/CategoryPage";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
+import SearchElement from '@/components/SearchElement';
+import Layout from "@/components/layout/Layout";
+import Background from "@/components/layout/Background";
 
 function App() {
   return (
     <>
       <Reset />
       <Router>
-        <Header />
+        <Background />
+        <SearchElement />
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/category/:categoryId" element={<CategoryPage />} />
+          <Route path="/" element={<Layout><Main /></Layout>} />
+          <Route path="/category/:categoryId" element={<Layout><CategoryPage /></Layout>} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
-        <Nav />
       </Router>
     </>
   );
