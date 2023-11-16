e1771cfa88d01e77b127ca889103ab631fedcb2e
jannyshim <29jannyy@gmail.com>
2023-11-16 18:27:01 +0900

    ✨ Feat: 카카오 로그인 1차 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "@pages/Main";
 import NotFound from "@pages/NotFound";
 import CategoryPage from "@pages/CategoryPage";
-import SearchElement from '@/components/SearchElement';
+import SearchElement from "@/components/SearchElement";
 import Layout from "@/components/layout/Layout";
 import Background from "@/components/layout/Background";
+import Mypage from "@/pages/Mypage";
+import Kakao from "@/pages/Kakao";
 
 function App() {
   return (
@@ -15,9 +17,39 @@ function App() {
         <Background />
         <SearchElement />
         <Routes>
-          <Route path="/" element={<Layout><Main /></Layout>} />
-          <Route path="/category/:categoryId" element={<Layout><CategoryPage /></Layout>} />
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Main />
+              </Layout>
+            }
+          />
+          <Route
+            path="/category/:categoryId"
+            element={
+              <Layout>
+                <CategoryPage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/auth"
+            element={
+              <Layout>
+                <Kakao />
+              </Layout>
+            }
+          />
           <Route path="/*" element={<NotFound />} />
+          <Route
+            path="/mypage"
+            element={
+              <Layout>
+                <Mypage />
+              </Layout>
+            }
+          />
         </Routes>
       </Router>
     </>
