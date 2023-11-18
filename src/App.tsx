4e1745ff5a8f3f65a5675f7e590c09f0a9452e8d
윤태현 <yoonth0919@gmail.com>
2023-11-19 01:26:39 +0900

    :art: Style: App.tsx 들여쓰기 및 import 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Reset } from "styled-reset";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "@pages/Main";
-import NotFound from "@pages/NotFound";
 import CategoryPage from "@pages/CategoryPage";
+import PostDetail from "@/pages/PostDetail";
+import Kakao from "@/pages/Kakao";
+import Map from "@/pages/Map";
+import Shop from "@/pages/Shop";
+import Community from "@/pages/Community";
+import LoginPage from "@/pages/LoginPage";
+import Mypage from "@/pages/Mypage";
+import NotFound from "@pages/NotFound";
+
 import SearchElement from "@/components/SearchElement";
 import Layout from "@/components/layout/Layout";
 import Background from "@/components/layout/Background";
-import Mypage from "@/pages/Mypage";
-import Kakao from "@/pages/Kakao";
-import Map from "@/pages/Map";
-import Community from "@/pages/Community";
-import Shop from "@/pages/Shop";
-import PostDetail from "@/components/PostDetail";
 
 function App() {
   return (
@@ -21,57 +23,16 @@ function App() {
         <Background />
         <SearchElement />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Main />
-              </Layout>
-            }
-          />
-          <Route
-            path="/category/:categoryId"
-            element={
-              <Layout>
-                <CategoryPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/auth"
-            element={
-              <Layout>
-                <Kakao />
-              </Layout>
-            }
-          />
-          <Route
-            path="/map"
-            element={
-              <Layout>
-                <Map />
-              </Layout>
-            }
-          />
-          <Route
-            path="/shop"
-            element={
-              <Layout>
-                <Shop />
-              </Layout>
-            }
-          />
+          <Route path="/" element={<Layout><Main /></Layout>} />
+          <Route path="/category/:categoryId" element={<Layout><CategoryPage /></Layout>} />
+          <Route path="/auth" element={<Layout><Kakao /></Layout>} />
+          <Route path="/map" element={<Layout><Map /></Layout>} />
+          <Route path="/shop" element={<Layout><Shop /></Layout>} />
           <Route path="/community" element={<Community />} />
-          <Route path="/post/:postId" element={<PostDetail />}></Route>
+          <Route path="/post/:postId" element={<PostDetail />} />
+          <Route path="/mypage" element={<Layout><Mypage /></Layout>} />
+          <Route path="/login" element={<Layout><LoginPage /></Layout>} />
           <Route path="/*" element={<NotFound />} />
-          <Route
-            path="/mypage"
-            element={
-              <Layout>
-                <Mypage />
-              </Layout>
-            }
-          />
         </Routes>
       </Router>
     </>
