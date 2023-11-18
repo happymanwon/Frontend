253b8529f614527c71d7483789586589276a1f6f
윤태현 <yoonth0919@gmail.com>
2023-11-19 01:25:22 +0900

    :label: Add: 전역 타입 관리 apidata.d.ts 파일 추가

---
+++ src/apidata.d.ts
@@ -0,0 +1,15 @@
+interface StoreItem {
+  base_ym: string;
+  induty_code_se: number;
+  induty_code_se_name: string;
+  sh_addr: string;
+  sh_id: null | string;
+  sh_info: string;
+  sh_name: string;
+  sh_phone: null | string;
+  sh_photo: string;
+  sh_pride: string;
+  sh_rcmn: number;
+  sh_way: string;
+  가격: null
+}
