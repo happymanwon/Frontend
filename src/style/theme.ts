c0a27195894e924cf919468f357629c6d220e154
jannyshim <29jannyy@gmail.com>
2023-11-24 12:23:25 +0900

    💄 Design: theme 컬러 파일 추가

---
+++ src/style/theme.ts
@@ -0,0 +1,15 @@
+const colors = {
+  grey: "#aab2b5",
+  lightGrey: "#E8E8E8",
+  darkGrey: "#4E5867",
+  mediumGreen: "#2AB673",
+  lightGreen: "#00BF18",
+  white: "#fff",
+  black: "#000",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
