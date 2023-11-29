ecde832cd6012bb4f8d108cb8820b28f98839354
윤태현 <yoonth0919@gmail.com>
2023-11-29 23:56:14 +0900

    ➕ Add: Map 페이지, fetching data 및 kakao map data type 파일 추가

---
+++ src/types/map/addressSearchType.ts
@@ -0,0 +1,38 @@
+export interface AddressSearchType {
+  address: Address;
+  address_name: string;
+  address_type: string;
+  road_address: RoadAddress;
+  x: string;
+  y: string;
+}
+
+interface Address {
+  address_name: string;
+  b_code: string;
+  h_code: string;
+  main_address_no: string;
+  mountain_yn: string;
+  region_1depth_name: string;
+  region_2depth_name: string;
+  region_3depth_h_name: string;
+  region_3depth_name: string;
+  sub_address_no: string;
+  x: string;
+  y: string;
+}
+
+interface RoadAddress {
+  address_name: string;
+  building_name: string;
+  main_building_no: string;
+  region_1depth_name: string;
+  region_2depth_name: string;
+  region_3depth_name: string;
+  road_name: string;
+  sub_building_no: string;
+  underground_yn: string;
+  x: string;
+  y: string;
+  zone_no: string;
+}
---
+++ src/types/map/storeDataType.ts
@@ -0,0 +1,8 @@
+export interface StoreDataType {
+  id: number;
+  category: number;
+  address: string;
+  imageUrl: string;
+  name: string;
+  way: string;
+}
