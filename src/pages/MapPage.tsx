b28ee471df711cf4c6eb30d9120c44b60bc7e3b1
윤태현 <yoonth0919@gmail.com>
2023-12-06 00:23:36 +0900

    ♻️ Refactor: 데이터 fetching 수정 및 지도에 마커, Drag 컴포넌트 생성

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -3,62 +3,132 @@ import { useQuery } from "@tanstack/react-query";
 
 import Card from "@/components/map/Card";
 import Map from "@/components/map/Map";
+import DraggableCardComponent from "@/components/map/DraggableCardComponent";
 
 import { useMapToggleStore } from "@stores/mapToggle";
 import useRegionStore from "@/stores/location";
 
-import { StoreDataType } from "@/types/map/storeDataType";
+import { ResultDataType } from "@/types/map/storeDataType";
 
 import { styled } from "styled-components";
+import { useEffect, useState } from "react";
+
+interface PositionError {
+  message: string;
+}
+
+interface CurrentLocation {
+  lat: number;
+  lng: number;
+}
 
 const MapPage = () => {
   const { isMap } = useMapToggleStore();
   const { districtId, district } = useRegionStore();
+  const [currentLocation, setCurrentLocation] =
+    useState<CurrentLocation | null>(null);
+  const [locationError, setLocationError] = useState<PositionError | null>(
+    null
+  );
 
   const getStoreAPI = async () => {
-    const { data } = await axios(`/api/shops`);
-    const filterData = data.filter((store: StoreDataType) => {
-      if (store.address.includes(district[districtId])) {
-        return store;
-      }
-    });
-    return filterData;
+    const { data } = await axios.get<ResultDataType>(
+      `/api/shops?localCode=${districtId}`
+    );
+    const filteredData = data.data.filter((shop) =>
+      shop.roadAddress.includes(district[districtId])
+    );
+    return filteredData;
   };
 
+  // api 요청
   const {
-    isLoading,
-    error,
+    isLoading: isStoreDataLoading,
+    error: storeDataError,
     data: storeData,
   } = useQuery({
     queryKey: ["storeData", districtId],
-    queryFn: () => getStoreAPI(),
+    queryFn: getStoreAPI,
   });
 
-  if (isLoading) return null;
-  if (error) return <div>{error.message}</div>;
+  // 현재 위치 찾기
+  const findCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setCurrentLocation({
+          lat: latitude,
+          lng: longitude,
+        });
+      },
+      (error) => {
+        setLocationError(error);
+      }
+    );
+  };
 
+  useEffect(() => {
+    findCurrentLocation();
+  }, []);
+
+  if (isStoreDataLoading || currentLocation === null) return null;
+  if (storeDataError || locationError) {
+    let errorMessage = "";
+
+    if (storeDataError && "message" in storeDataError)
+      errorMessage = storeDataError.message;
+    if (locationError && "message" in locationError)
+      errorMessage = locationError.message;
+
+    return <div>{errorMessage}</div>;
+  }
   return (
     <MapContainer>
       {isMap ? (
-        <Map isMap={isMap} storeData={storeData} />
+        <>
+          <Map
+            isMap={isMap}
+            storeData={storeData!}
+            currentLocation={currentLocation}
+            setCurrentLocation={setCurrentLocation}
+          />
+          <DraggableCardComponent storeData={storeData!} />
+        </>
       ) : (
-        <CardContainer>
-          <Card storeData={storeData} />
-        </CardContainer>
+        <CardMain>
+          <Card storeData={storeData!} />
+        </CardMain>
       )}
     </MapContainer>
+    // <MapContainer>
+    //   <Map
+    //     isMap={isMap}
+    //     storeData={storeData!}
+    //     currentLocation={currentLocation}
+    //     setCurrentLocation={setCurrentLocation}
+    //   />
+    //   <DraggableContainer />
+    // </MapContainer>
   );
 };
 
 const MapContainer = styled.main`
   width: 100%;
   height: calc(100vh - 6.125rem - 4.5rem);
+  position: relative;
+  overflow: hidden;
 `;
 
-const CardContainer = styled.div`
+const CardMain = styled.div`
   height: calc(100vh - 6.125rem - 4.5rem);
   padding: 0 1.75rem;
   overflow: auto;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export default MapPage;
