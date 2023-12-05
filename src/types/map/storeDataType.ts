f9cc6757743e4e51124f7d06c8dfdbe4da79f33d
윤태현 <yoonth0919@gmail.com>
2023-12-06 00:22:44 +0900

    ♻️ Refactor: Store 값에 대한 타입 수정

--- src/types/map/storeDataType.ts
+++ src/types/map/storeDataType.ts
@@ -1,8 +1,17 @@
 export interface StoreDataType {
-  id: number;
-  category: number;
   address: string;
+  category: number;
+  id: number;
   imageUrl: string;
+  latitude: string;
+  longitude: string;
   name: string;
+  roadAddress: string;
   way: string;
+}
+
+export interface ResultDataType {
+  code: number;
+  data: StoreDataType[];
+  message: string;
 }
