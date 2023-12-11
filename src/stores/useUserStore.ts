6f7a1c76c32ee4f0cd8bc8d4e5b91e8449c9d604
윤태현 <yoonth0919@gmail.com>
2023-12-12 00:58:07 +0900

    ➕ Add: user Zustand 관리 파일 추가

---
+++ src/stores/useUserStore.ts
@@ -0,0 +1,33 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+interface useUserStoreType {
+  accessToken: string | null;
+  refreshToken: string | null;
+  memberId: number | null;
+  nickname: string | null;
+  setLoginData: (data: {
+    accessToken: string | null;
+    refreshToken: string | null;
+    memberId: number | null;
+    nickname: string | null;
+  }) => void;
+}
+
+// 1. Zustand 스토어 생성 및 로컬 스토리지와의 동기화 설정
+const useUserStore = create(
+  persist<useUserStoreType>(
+    (set) => ({
+      accessToken: null,
+      refreshToken: null,
+      memberId: null,
+      nickname: null,
+      setLoginData: (data) => set({ ...data }),
+    }),
+    {
+      name: "user-store", // 로컬 스토리지에 저장될 때 사용될 키
+    }
+  )
+);
+
+export default useUserStore;
