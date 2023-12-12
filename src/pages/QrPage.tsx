8ab780074210c5fc95cc05a7a0278509e25052fd
윤태현 <yoonth0919@gmail.com>
2023-12-12 23:23:54 +0900

    💄 Design: QR 페이지 마크업

---
+++ src/pages/QrPage.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const QrPage = () => {
+  return <QrContainer></QrContainer>;
+};
+
+const QrContainer = styled.header`
+  height: 6.125rem;
+  width: 100%;
+`;
+
+export default QrPage;
